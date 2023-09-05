dafe2f67a6b01e636cb1709b839d33680f8ecb62
manaswiniray <manaswiniray2002@gmail.com>
2023-09-05 23:38:54 +0530

    Committed Adapter Pattern

--- AdapterPattern/Program.cs
+++ AdapterPattern/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            StringToIntConverter converter = new StringToIntConverter();
+
+            IConverter adapter = new StringToIntAdapter(converter);
+
+            Console.WriteLine("Enter your value for Conversion: ");
+            string userInput=Console.ReadLine();
+
+            int convertedResult=adapter.Converter(userInput);
+            if (convertedResult == -1 )
+            {
+                Console.WriteLine("No Integer Value Exists for this Inputted String");
+                return;
+            }
+            Console.WriteLine("The result is: {0}", convertedResult);
         }
     }
 }
---
+++ AdapterPattern/StringToIntConverter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdapterPattern
+{
+    public class StringToIntConverter
+    {
+        public int ConvertStringToInt(string str)
+        {
+            try
+            {
+                return Convert.ToInt32(str);
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+    }
+}
